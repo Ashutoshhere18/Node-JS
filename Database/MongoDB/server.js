bc412ba0c417038731fc35d05bb77069966e0890
Ashutoshhere18 <mishraashutoshwk@gmail.com>
2025-12-18 09:44:56 +0530

    18/12/2025

--- Database/MongoDB/server.js
+++ Database/MongoDB/server.js
@@ -1,6 +1,9 @@
 import express from "express"
 import {MongoClient} from "mongodb"
 
+const app=express();
+app.use(express.json())
+
 const client = new MongoClient("mongodb://127.0.0.1:27017");
 
 const connectDB=async()=>{
@@ -26,8 +29,22 @@ const addStudent=async()=>{
 const readStudent=async()=>{
 const db=await connectDB();
 const data=await db.collection("student").find().toArray();
-console.log(data);
-// return data;
+// console.log(data[0]._id.toJSON());
+return data;
 };
-addStudent();
-console.log(readStudent());
+
+
+app.get("/api",async(req,res)=>{
+    const  data= await readStudent();
+    res.json(data);
+});
+
+app.post("/api",async(req,res)=>{
+  const data=await addStudent();
+  const record=req.body=data;
+  res.json(record);
+})
+
+app.listen(4000,()=>{
+    console.log("Server Started at 4000...");
+})
