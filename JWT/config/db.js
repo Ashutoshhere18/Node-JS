4f9fe3a4379e22a2d4ac8031ca167571b18a70f9
Ashutoshhere18 <mishraashutoshwk@gmail.com>
2026-01-05 12:20:01 +0530

    05/01/2026

---
+++ JWT/config/db.js
@@ -0,0 +1,10 @@
+import comgoose from 'mongoose'
+
+export const connectDB=async()=>{
+  try{
+ mongoose.connect('mongodb://localhost:27017/User');
+ console.log("MongoDB connected successfully!.");
+  }catch(err){
+console.log("MongoDB Not connected !.",err);
+  }
+} ;
