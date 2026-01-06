f0656ff70821646dccb14eef67dc32d4abb99aad
Ashutoshhere18 <mishraashutoshwk@gmail.com>
2026-01-06 21:42:46 +0530

    06/01/2026

--- passportJS/controllers/AuthController.js
+++ passportJS/controllers/AuthController.js
@@ -1,4 +1,33 @@
-import {userModel}  from '../models/AuthModel.js'
+import {Users}  from '../models/AuthModel.js'
+import bcrypt from 'bcrypt'
 
+export const signup=async(req,res)=>{
 
+try{
+    const {email,password}=req.body;
+    const hashedPassword=await bcrypt.hash(password,10);
+    Users.create({
+        email,password:hashedPassword
+    });
+    res.json({message:"User Registered!.."});
+    
+}catch(err){
+   res.json({message:"User not registered!.."})
+}
 
+}
+
+export const signin=async(req,res)=>{
+    res.json({message:"User SignIn successfully!",user:req.user});
+}
+
+export const signout=async(req,res)=>{
+    req.logout(()=>{
+        res.json({message:"Logout success!.."});
+    });
+};
+
+export const home=async(req,res)=>{
+    res.json({message:"Home Page accessed!",user:req.user});
+
+}
