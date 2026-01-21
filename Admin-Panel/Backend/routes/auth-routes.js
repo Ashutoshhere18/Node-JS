8b65a484965b775a93f0147bd623b16b3d0c0c64
Ashutoshhere18 <mishraashutoshwk@gmail.com>
2026-01-21 22:02:38 +0530

    21/01/2026

--- Admin-Panel/Backend/routes/auth-routes.js
+++ Admin-Panel/Backend/routes/auth-routes.js
@@ -1,11 +1,14 @@
 import express from 'express'
-import {signup,signin,signout,verifyOTP,checkLoginStatus} from '../controllers/auth-controller.js'
+import {signup,signin,signout,verifyOTP,checkLoginStatus,changePassword,forgotPassword,changeForgotPassword} from '../controllers/auth-controller.js'
 
 const router=express.Router();
 
 router.post("/signup",signup);
 router.post("/signin",signin);
 router.post("/verifyOtp",verifyOTP);
+router.post("/changePassword",changePassword);
+router.post("/forgotPassword",forgotPassword);
+router.post("/changeForgotPassword",changeForgotPassword);
 
 router.get("/signout",signout);
 router.get("/checkLoginStatus",checkLoginStatus);
