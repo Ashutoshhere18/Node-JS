32e1e91c45930eaf8f6d4f10e6312744faf0eb3c
Ashutoshhere18 <mishraashutoshwk@gmail.com>
2026-01-28 10:45:38 +0530

    28/01/2026

--- Admin-Panel/Frontend/src/ForgotPassword.jsx
+++ Admin-Panel/Frontend/src/ForgotPassword.jsx
@@ -1,31 +1,4 @@
-// import React,{useState} from 'react'
-// import axios from 'axios'
-// import {useNavigate} from 'react-router'
-// import { base_uri } from '../utils/global-function';
 
-// export default function ForgotPassword() {
-
-//     const[email,setEmail]=useState("");
-//     const Navigate=useNavigate();
-
-//     const handleForgotPassword=async()=>{
-//        try{
-//          const res = await axios.post(`${base_uri}/auth/forgotPassword`, {email});
-//          alert(res.data.message);
-//        if(res.data.status){
-//         Navigate("/ChangeForgotPassword");
-//        }
-//        }catch(err){
-//         alert(err.message);
-//        }
-//     }
-//   return (
-//     <div>
-//    <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)} />
-//    <button onClick={handleForgotPassword} className='btn btn-primary'>Forgot Password</button>
-//     </div>
-//   )
-// }
 import React, { useState } from 'react'
 import axios from 'axios'
 import { useNavigate } from 'react-router'
