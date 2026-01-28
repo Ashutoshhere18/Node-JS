9002f13957f48c6fc1f354a4033ccad2469bec24
Ashutoshhere18 <mishraashutoshwk@gmail.com>
2026-01-28 10:43:37 +0530

    28/01/2026

--- Admin-Panel/Frontend/src/App.jsx
+++ Admin-Panel/Frontend/src/App.jsx
@@ -4,8 +4,9 @@ import {Routes,Route} from 'react-router'
 import Signin from './Signin'
 import Signup from './Signup'
 import ForgotPassword from './ForgotPassword'
-// import VerifyOtp from 'VerifyOtp'
-
+import ChangeForgotPassword from './ChangeForgotPassword'
+import VerifyOtp from './Verifyotp'
+import ProfilePage from './ProfilePage'
 
 function App() {
  
@@ -15,8 +16,10 @@ function App() {
       <Routes>
         <Route path='/' element={<Signin/>}/>
         <Route path='/signup' element={<Signup/>}/>
-        {/* <Route path='/verify-otp' element={<VerifyOtp/>}/> */}
+        <Route path='/verify-otp' element={<VerifyOtp/>}/>
         <Route path='/forgotPassword' element={<ForgotPassword/>}/>
+        <Route path='/ChangeForgotPassword' element={<ChangeForgotPassword/>}/>
+        <Route path='/ProfilePage' element={<ProfilePage/>}/>
       </Routes>
     </>
   )
