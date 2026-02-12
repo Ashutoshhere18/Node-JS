5812c4930d7dfa6709b5c6ef9239281f76aec559
Ashutoshhere18 <mishraashutoshwk@gmail.com>
2026-02-12 23:56:28 +0530

    12/02/2026

---
+++ Admin-Panel/Frontend/src/components/AddEmployee.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+export default function AddEmployee() {
+  return (
+  <div className='container'>
+        <div className='shadow-lg'>
+      
+    <div className="mb-3">
+    <label for="exampleInputEmail1" className="form-label">Email address</label>
+    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+  </div>
+
+   <div className="mb-3">
+    <label for="exampleInputEmail1" className="form-label">Password</label>
+    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <div id="emailHelp" className="form-text">We'll never share your password with anyone else.</div>
+  </div>
+
+ <button className='btn btn-primary'> Add Employee</button>
+    </div>
+
+    <div className='mt-5 shadow-lg'>
+    <table class="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Name</th>
+      <th scope="col">Phone</th>
+      <th scope="col">Role</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+    </tr>
+    
+  </tbody>
+</table>
+    </div>
+   
+  </div>
+  )
+}
--- Admin-Panel/Frontend/src/pages/HomePage.jsx
+++ Admin-Panel/Frontend/src/pages/HomePage.jsx
@@ -12,8 +12,8 @@ export default function Dashboard() {
 
           <ul className="nav flex-column gap-2">
             <li className="nav-item">
-              <Link to="/dashboard" className="nav-link text-white">
-                📊 Dashboard
+              <Link to="/Add-employee" className="nav-link text-white">
+                Employee
               </Link>
             </li>
             <li className="nav-item">
